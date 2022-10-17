58179d99b335c2670d47bad7853f4833d403484e
MadScientist586 <Mr.Scientist.Knight@gmail.com>
2022-10-17 12:39:44 -0500

    finished the exercises

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Aiden";
+let lastName;
+var age;
 
 // Exercise 2
-
+lastName = "Watson-Knight";
+age = 13;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = M;
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
